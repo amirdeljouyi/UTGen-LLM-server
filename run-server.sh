e9b09ae91d21ed73df286ab351ed08b0f395f31d
microtestcarver <amirdeljouyi@gmail.com>
2024-01-30 10:11:15 +0100

    update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -5,4 +5,4 @@ source .env/bin/activate
 logDir="log/"
 
 mkdir -p $logDir
-strawberry server main > $logDir\$(date '+%Y-%m-%d-%H-%M').log
+strawberry server main > $logDir/$(date '+%Y-%m-%d-%H-%M').log
