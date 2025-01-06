f775dddd80ff18240af13add3a84581968c71422
amirdeljouyi <amirdeljouyi@gmail.com>
2025-01-06 14:45:57 +0100

    update run-docker-server.sh

--- run-docker-server.sh
+++ run-docker-server.sh
@@ -1,5 +1,5 @@
 docker exec ollama ollama pull codellama:7b-instruct
-docker exec -it ollama ollama run codellama:7b-instruct
+docker exec -it ollama ollama run codellama:7b-instruct /bye
 
 export OLLAMA_API_URL=http://ollama:11434/api/generate
 
